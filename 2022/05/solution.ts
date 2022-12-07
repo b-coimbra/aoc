f92568e3ccbf9ec3c8f51e0523b7550d59f287f7
Bruno Oliveira <brunocoi@iadb.org>
2022-12-07 17:04:07 -0300

    Add day 05 part 2

--- 2022/05/solution.ts
+++ 2022/05/solution.ts
@@ -7,7 +7,7 @@ type Crate = string[];
 type Move  = { from: number; to: number };
 
 const isNum = (x: any) => /\d/.test(x);
-const isChar = (x: any) => !isNum(x) && /\w/.test(x);
+const isChar = (x: any) => !isNum(x) && /\w/.test(x) && x != undefined;
 
 const [crates, procedures] = input
     .split("\n\n")
@@ -23,14 +23,20 @@ const base = crates[crates.length - 1];
 
 const moves = procedures.flatMap(parse).reverse();
 
-const move = (stack: Stack, position: Move): Stack => {
-    stack[position.to]?.push(stack[position.from].pop()!)
+const move = (stack: Stack, position: Move, multiple: boolean): Stack => {
+    if (!stack) return;
+
+    if (multiple)
+        stack[position.to]?.unshift(stack[position.from]?.shift());
+    else
+        stack[position.to]?.push(stack[position.from]?.pop()!);
+
     return stack;
 };
 
-const rearrange = (stack: Stack, moves: Move[]): Stack => {
+const rearrange = (stack: Stack, moves: Move[], multiple = false): Stack => {
     if (!moves.length) return stacks;
-    return rearrange(move(stack, moves.pop()!), moves);
+    return rearrange(move(stack, moves.pop()!, multiple), moves, multiple);
 };
 
 const nthCrate = (n): Crate => crates
@@ -43,8 +49,17 @@ const stacks = base.split('').reduce((acc, crate, i) => {
     return acc;
 }, [] as Stack);
 
-let answer = rearrange(stacks, moves)
+// let answer = rearrange(stacks, moves)
+//     .map(x => x.pop())
+//     .join('');
+
+// console.log(answer);
+
+//// PART 2
+
+let answer = rearrange(stacks, moves, true)
     .map(x => x.pop())
-    .join('');
+    .reverse()
+    .join('')
 
 console.log(answer);
